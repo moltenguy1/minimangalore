379d8c94490ffc7c14be7eac104279792c9ae0e3
BawooiT <BawooiT@d1c0eb94-fc07-11dd-a7be-4b3ef3b0700c>
2009-02-20 11:24:51 +0000

    Player Input 수정

--- trunk/ minimangalore/code/demo/properties/playerinputproperty.cc
+++ trunk/ minimangalore/code/demo/properties/playerinputproperty.cc
@@ -16,7 +16,10 @@
 #include "msg/movedirection.h"
 #include "msg/movegoto.h"
 #include "msg/movestop.h"
+#include "msg/moveturn.h"
+#include "msg/moverotate.h"
 #include "msg/playerstatus.h"
+#include "msg/updatetransform.h"
 
 #include "game/time/inputtimesource.h"
 
@@ -52,104 +55,95 @@ PlayerInputProperty::OnBeginFrame()
 {
     // only do something if we have the input focus
     if (FocusManager::Instance()->GetInputFocusEntity() == GetEntity())
-    {        
+    {
         this->HandleInput();
-    }	
+	}
     InputProperty::OnBeginFrame();
 }
 
 void
 PlayerInputProperty::SendMove(const vector3& dir)
 {
-        // send a MoveGoto to the mouse intersection point
-        //const vector3& mousePos = envQueryMgr->GetMousePos3d();
+    // send a MoveGoto to the mouse intersection point
+    //const vector3& mousePos = envQueryMgr->GetMousePos3d();
 
-        const vector3& from = GetEntity()->GetMatrix44(Attr::Transform).pos_component();
+    const vector3& from = GetEntity()->GetMatrix44(Attr::Transform).pos_component();
 
-		Ptr<Message::MoveGoto> msg = Message::MoveGoto::Create();
-        
-		vector3 to = from;
-		to += dir;
-		msg->SetPosition(to);
-		
-		GetEntity()->SendAsync(msg);
+	Ptr<Message::MoveGoto> msg = Message::MoveGoto::Create();
+    
+	vector3 to = from;
+	to += dir;
+	msg->SetPosition(to);
+	
+	GetEntity()->SendAsync(msg);
 
-        // record the current time for the HandleLMBPressed() method
-        this->moveGotoTime = InputTimeSource::Instance()->GetTime();
+    // record the current time for the HandleLMBPressed() method
+    this->moveGotoTime = InputTimeSource::Instance()->GetTime();
 }
 
-//-----------------------------------------------------------------------------
-/**
-*/
-void
-PlayerInputProperty::HandleInput()
+//////////////////////////////////////////////////////////////////////////
+// void SendTurn(const vector3& dir)
+//////////////////////////////////////////////////////////////////////////
+void PlayerInputProperty::SendTurn(const float dir)
+{
+	Ptr<Message::MoveRotate> msg = Message::MoveRotate::Create();
+	Ptr<Message::UpdateTransform> Updatemsg = Message::UpdateTransform::Create();
+
+	matrix44 mat = GetEntity()->GetMatrix44(Attr::Transform);
+
+	float c = n_cos(dir);
+	float s = n_sin(dir);
+	int i;
+	for( i=0 ; i<4 ; i++ )
+	{
+		float mi0 = mat.m[i][0];
+		float mi2 = mat.m[i][2];
+		mat.m[i][0] = mi0*c + mi2*s;
+		mat.m[i][2] = mi0*-s + mi2*c;
+	}
+
+	Updatemsg->SetMatrix(mat);
+
+	GetEntity()->SendAsync(Updatemsg);
+
+	this->moveGotoTime = InputTimeSource::Instance()->GetTime();
+}
+
+//////////////////////////////////////////////////////////////////////////
+// void HandleInput()
+//////////////////////////////////////////////////////////////////////////
+void PlayerInputProperty::HandleInput()
 {
     Input::Server* inputServer = Input::Server::Instance();
 
-	/*
-    const float vel = 1.0f;
-    static const vector3 left(-vel, 0.0f, 0.0f);
-    static const vector3 right(vel, 0.0f, 0.0f);
-    static const vector3 up(0.0f, 0.0f, vel);
-    static const vector3 down(0.0f, 0.0f, -vel);
-    bool moved = false;
-
-    if (inputServer->GetButtonDown("moveLeft"))
-    {
-        Ptr<Message::MoveDirection> msg = Message::MoveDirection::Create();
-        msg->SetCameraRelative(true);
-        msg->SetDirection(left);
-        GetEntity()->SendAsync(msg);
-        moved = true;
-    }
-    if (inputServer->GetButtonDown("moveRight"))
-    {
-        Ptr<Message::MoveDirection> msg = Message::MoveDirection::Create();
-        msg->SetCameraRelative(true);
-        msg->SetDirection(right);
-        GetEntity()->SendAsync(msg);
-        moved = true;
-    }
-    if (inputServer->GetButtonDown("moveBackward"))
-    {
-        Ptr<Message::MoveDirection> msg = Message::MoveDirection::Create();
-        msg->SetCameraRelative(true);
-        msg->SetDirection(up);
-        GetEntity()->SendAsync(msg);
-        moved = true;
-    }
-    if (inputServer->GetButtonDown("moveForward"))
-    {
-        Ptr<Message::MoveDirection> msg = Message::MoveDirection::Create();
-        msg->SetCameraRelative(true);
-        msg->SetDirection(down);
-        GetEntity()->SendAsync(msg);
-        moved = true;
-    }
-
-    if (!moved)
-    {
-        Ptr<Message::MoveStop> msg = Message::MoveStop::Create();
-        GetEntity()->SendAsync(msg);
-    }
-	*/
-
 	this->playerMsg.Clear();
 
-if (inputServer->GetButtonPressed("mouseMMB"))
-{
-    // get horizontal and vertical mouse movement
-    //Input::Server* inputServer = Input::Server::Instance();
-    float x = inputServer->GetSlider("mouseLeft") - inputServer->GetSlider("mouseRight");
-    float y = inputServer->GetSlider("mouseUp") - inputServer->GetSlider("mouseDown");
+	if (inputServer->GetButtonPressed("mouseLMB"))
+	{
+		// get horizontal and vertical mouse movement
+		//Input::Server* inputServer = Input::Server::Instance();
+		float x = inputServer->GetSlider("mouseLeft") - inputServer->GetSlider("mouseRight");
+		float y = inputServer->GetSlider("mouseUp") - inputServer->GetSlider("mouseDown");
 
-    // create CameraOrbit message
-	Ptr<Message::CameraOrbit> camMsg = Message::CameraOrbit::Create();
-    camMsg->SetHorizontalRotation(x);
-    camMsg->SetVerticalRotation(y);
-    GetEntity()->SendAsync(camMsg);
-}
+		// create CameraOrbit message
+		Ptr<Message::CameraOrbit> camMsg = Message::CameraOrbit::Create();
+		camMsg->SetHorizontalRotation(x);
+		camMsg->SetVerticalRotation(y);
+		GetEntity()->SendAsync(camMsg);
+	}
 
+	if( inputServer->GetButtonPressed("mouseRMB") )
+	{
+		float x = inputServer->GetSlider("mouseLeft") - inputServer->GetSlider("mouseRight");
+		float y = inputServer->GetSlider("mouseUp") - inputServer->GetSlider("mouseDown");
+
+		Ptr<Message::CameraOrbit> camMsg = Message::CameraOrbit::Create();
+		camMsg->SetHorizontalRotation(x);
+		camMsg->SetVerticalRotation(y);
+		GetEntity()->SendAsync(camMsg);
+
+		//this->SendTurn();
+	}
 
 	const float velocity = 1.5f;
 
@@ -164,6 +158,19 @@ if (inputServer->GetButtonPressed("mouseMMB"))
 		if ((curTime - this->moveGotoTime) > 0.25f)
 		{
             this->SendMove(vector3(0.0f, 0.0f, -velocity));
+		}
+	}
+
+	if( inputServer->GetButtonDown("moveBackward"))
+	{
+		this->SendMove(vector3(0.0f, 0.0f, velocity));
+	}
+	else if( inputServer->GetButtonPressed("moveBackward"))
+	{
+		nTime curTime = InputTimeSource::Instance()->GetTime();
+		if ((curTime - this->moveGotoTime) > 0.25f)
+		{
+			this->SendMove(vector3(0.0f, 0.0f, velocity));
 		}
 	}
 
